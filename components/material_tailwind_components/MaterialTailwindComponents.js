c3b6e87f2ca445c25d4414d569d028ebff1cfd1d
lakshankd <dominiclakshank@gmail.com>
2024-04-07 13:02:11 +0530

    cofigure material tailwind

---
+++ components/material_tailwind_components/MaterialTailwindComponents.js
@@ -0,0 +1,4 @@
+"use client";
+import { ThemeProvider, Button, Typography } from "@material-tailwind/react";
+
+export { ThemeProvider, Button, Typography };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "cdn.pixabay.com"], // Replace with the domain of the image
+  },
+};
 
 export default nextConfig;
